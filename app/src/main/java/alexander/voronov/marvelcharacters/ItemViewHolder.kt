aebae72e463752bae06ea5830829a996d8a80fc4
Alexander Voronov <dr.strum@gmail.com>
2022-09-29 13:10:41 +0300

    Added ItemViewHolder.kt with Coil library

--- app/src/main/java/alexander/voronov/marvelcharacters/ItemViewHolder.kt
+++ app/src/main/java/alexander/voronov/marvelcharacters/ItemViewHolder.kt
@@ -1,10 +1,25 @@
 package alexander.voronov.marvelcharacters
 
+import alexander.voronov.marvelcharacters.databinding.ElementRecyclerviewBinding
 import alexander.voronov.marvelcharacters.entities.Result
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 
-class ItemViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(itemView) {
+class ItemViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context)
+        .inflate(R.layout.element_recyclerview, parent, false)
+) {
+    private val binding = ElementRecyclerviewBinding.bind(itemView)
 
-    fun bind(itemEntity: Result) {}
+    fun bind(itemEntity: Result) {
+
+        val thumbnailLink: String =
+            itemEntity.thumbnail.path + "." + itemEntity.thumbnail.extension
+
+        binding.thumbnailImageView.load(thumbnailLink)
+        binding.itemIdTextView.text = itemEntity.id.toString()
+        binding.itemNameTextView.text = itemEntity.name
+    }
 }
