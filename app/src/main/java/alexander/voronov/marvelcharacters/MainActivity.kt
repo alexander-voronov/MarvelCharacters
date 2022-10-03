6e207030ce868fbf63e75322fdc6717f7ba4ca41
Alexander Voronov <dr.strum@gmail.com>
2022-10-03 16:04:31 +0300

    Added progress bar

--- app/src/main/java/alexander/voronov/marvelcharacters/MainActivity.kt
+++ app/src/main/java/alexander/voronov/marvelcharacters/MainActivity.kt
@@ -4,20 +4,32 @@ import alexander.voronov.marvelcharacters.databinding.ActivityMainBinding
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val adapter = ItemsAdapter()
-    private val itemsRepository: ItemsRepository
+    private val itemsRepository: ItemsRepository = RetrofitItemsRepositoryImplementation()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        showProgress(false)
 
         binding.activityMainLoadButton.setOnClickListener {
-            Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+            showProgress(true)
+            itemsRepository.getItems(
+                onSuccess = {
+                    showProgress(false)
+                    adapter.setData(it)
+                },
+                onError = {
+                    showProgress(false)
+                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                }
+            )
         }
 
         initRecyclerView()
@@ -26,8 +38,10 @@ class MainActivity : AppCompatActivity() {
     private fun initRecyclerView() {
         binding.itemRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.itemRecyclerView.adapter = adapter
-        itemsRepository.getItems {
-            adapter.setData()
-        }
+    }
+
+    private fun showProgress(inProgress: Boolean) {
+        binding.progressBar.isVisible = inProgress
+        binding.itemRecyclerView.isVisible = !inProgress
     }
 }
--- app/src/main/java/alexander/voronov/marvelcharacters/RetrofitItemsRepositoryImplementation.kt
+++ app/src/main/java/alexander/voronov/marvelcharacters/RetrofitItemsRepositoryImplementation.kt
@@ -11,6 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.security.MessageDigest
 
+
 class RetrofitItemsRepositoryImplementation : ItemsRepository {
 
     private val api = Retrofit.Builder()
